204b53bb500f8b4c3d64bd8d526775850f685443
mdabdullahbaloch <abdullah356abdulhameed@gmail.com>
2023-10-31 14:35:18 +0500

    first commit

---
+++ 1st Class/Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144
+let accountemail = "abdullah@gmail.com"
+var accountpassword = "12345"
+accountcity = "Karachi"
+// accountId = 2
+// accountemail ="baloch@gmail.com"
+// accountpassword = "987"
+// accountcity = "hyb"
+let accountstates
+console.log(accountId);
+/*
+prefer not use var
+because of issue in block scop and functional scope
+*/
+console.table([accountId, accountemail, accountpassword, accountcity, accountstates]);
+
